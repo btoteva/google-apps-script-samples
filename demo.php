acf4152b0e2aa2652f14807822d7c3166792f351
btoteva <btoteva@gmail.com>
2017-08-16 13:55:23 +0300

    Update demo.php

--- demo.php
+++ demo.php
@@ -6,7 +6,7 @@
 <h1>Google App Script  MailToReport Demo</h1>
 <div style='float:left; border:solid 0px red; padding:1em; background-color:transparent;'>
   <frame>
-      <?php include 'frame.html';?>
+      <?php include 'demo_frame.html';?>
   </frame>
  </div>
  </body>
